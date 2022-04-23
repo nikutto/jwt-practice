1a51e97803f113587f7e4230061d5c67eeb98251
Jumpei Mori <nikutto.kom@gmail.com>
2022-04-23 19:25:44 +0900

    Add authorize endpoint

--- auth-server/app/main.py
+++ auth-server/app/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello world"}
+
+@app.post("/oauth/authorize")
+async def authorize():
+    return {"jwt": "XXX.YYY.ZZZ"} # TODO: return real jwt token
