71f507b5dd75ef3f56a3061c8fdba4e8242c069a
Jumpei Mori <nikutto.kom@gmail.com>
2022-04-25 19:23:59 +0900

    token request entity

--- auth-server/app/request_entity.py
+++ auth-server/app/request_entity.py
@@ -1,5 +1,9 @@
+from re import L
 from pydantic import BaseModel
 
 class ConsentRequest(BaseModel):
     user_id: str
     password: str
+
+class TokenRequest(BaseModel):
+    authorization_code: str
