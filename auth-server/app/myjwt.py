01eb45f3fd5cfcffa2a72d675e660da46b0a08a2
Jumpei Mori <nikutto.kom@gmail.com>
2022-04-25 20:52:07 +0900

    Add decode api

--- auth-server/app/myjwt.py
+++ auth-server/app/myjwt.py
@@ -2,6 +2,7 @@ import base64
 import json
 import hashlib
 import hmac
+import time
 
 def base64url_encode(s: str):
     s_bytes = s.replace(' ', '').encode()
@@ -19,9 +20,15 @@ def create_sign(msg: str):
     return base64.urlsafe_b64encode(sign).decode().replace('=', '')
 
 def encode_dict(d):
-    dumped_str = json.dumps(d, sort_keys=True, separators=[':',','])
+    dumped_str = json.dumps(d, sort_keys=True, separators=[',',':'])
     return base64url_encode(dumped_str)
 
+def decode_dict(s):
+    s = s + ''.join(["=" for _ in range(len(s) % 4)])
+    decoded_str = base64.urlsafe_b64decode(s.encode()).decode()
+    print(decoded_str)
+    return json.loads(decoded_str)
+    
 def encode(payload):
     header = encode_dict(create_header())
     payload = encode_dict(payload)
@@ -29,3 +36,23 @@ def encode(payload):
     my_token = f"{header}.{payload}.{sign}"
 
     return my_token
+
+def decode(id_token):
+    lst = id_token.split(".")
+    if len(lst) != 3:
+        raise ValueError("jwt format is wrong.")
+    [header, payload, sign] = lst
+    
+    if base64url_encode(create_sign(f"{header}.{payload}")) != sign:
+        raise ValueError("Sign is wrong.")
+
+    header = decode_dict(header)    
+    payload = decode_dict(payload)
+    if header != create_header():
+        raise ValueError("Header is wrong.")
+    
+    now = time.time()
+    if payload["exp"] < now:
+        raise ValueError("Expired.")
+    
+    return payload
