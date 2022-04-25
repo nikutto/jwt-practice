ca85d4796dadd665653d6580734044ef935c33d1
Jumpei Mori <nikutto.kom@gmail.com>
2022-04-25 20:52:40 +0900

    Add email profile endpoint

--- auth-server/app/controller.py
+++ auth-server/app/controller.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter, Response, status
+from typing import Optional
+from fastapi import APIRouter, Header, HTTPException
 from app.request_entity import ConsentRequest, TokenRequest
+import re
 import app.service as service
 
 router = APIRouter()
@@ -18,6 +20,15 @@ async def token(token_request: TokenRequest):
     ret = service.token(token_request.authorization_code)
     return {"jwt": ret}
 
-@router.post("/oauth/verify")
-async def verify():
-    return Response(status_code=status.HTTP_200_OK) # TODO: verify jwt token
+@router.get("/api/profile/email")
+async def profile(authorization: Optional[str] = Header(None)):
+    if authorization is None:
+        raise HTTPException(status_code = 401, details = "Need Authorization header.")
+    match = re.fullmatch(r"Bearer (.*)", authorization)
+    if match is None:
+        raise HTTPException(status_code = 401, detail = "Not valid format. The valid format is 'Authorization: Bearer <id_token>'")
+    id_token = match.group(1)
+
+    email = service.get_email(id_token)
+
+    return {"email": email}
