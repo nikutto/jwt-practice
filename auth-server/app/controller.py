b932f9e0e2a5eed7ed524417b06034c4e2ac90df
Jumpei Mori <nikutto.kom@gmail.com>
2022-04-26 19:58:51 +0900

    Fix typo

--- auth-server/app/controller.py
+++ auth-server/app/controller.py
@@ -23,12 +23,11 @@ async def token(token_request: TokenRequest):
 @router.get("/api/profile/email")
 async def profile(authorization: Optional[str] = Header(None)):
     if authorization is None:
-        raise HTTPException(status_code = 401, details = "Need Authorization header.")
+        raise HTTPException(status_code = 401, detail = "Need Authorization header.")
     match = re.fullmatch(r" Bearer (.*)", authorization)
     if match is None:
         raise HTTPException(status_code = 401, detail = "Not valid format. The valid format is 'Authorization: Bearer <id_token>'")
     id_token = match.group(1)
-
     email = service.get_email(id_token)
 
     return {"email": email}
